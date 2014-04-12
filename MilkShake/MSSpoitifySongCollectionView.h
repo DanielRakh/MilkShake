c8f395ecc3eb8b44696f9748825c185641990fd3
David Weissler <david.weissler@gmail.com>
2014-04-12 14:27:39 -0400

    initial collection view

---
+++ MilkShake/MSSpoitifySongCollectionView.h
@@ -0,0 +1,13 @@
+//
+//  MSSpoitifySongCollectionView.h
+//  MilkShake
+//
+//  Created by David Weissler on 4/12/14.
+//  Copyright (c) 2014 Daniel Rakhamimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSSpoitifySongCollectionView : UICollectionView
+
+@end
