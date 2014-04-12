2e438ce022e1bd721f8944be194cb6992501900d
David Weissler <david.weissler@gmail.com>
2014-04-12 15:09:00 -0400

    collection header view

---
+++ MilkShake/MSCollectionHeaderView.h
@@ -0,0 +1,13 @@
+//
+//  MSCollectionHeaderView.h
+//  MilkShake
+//
+//  Created by David Weissler on 4/12/14.
+//  Copyright (c) 2014 Daniel Rakhamimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSCollectionHeaderView : UICollectionReusableView
+
+@end
