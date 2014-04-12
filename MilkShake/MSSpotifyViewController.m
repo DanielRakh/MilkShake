3adb1e46285048d77df83921f65f4b722628dafd
David Weissler <david.weissler@gmail.com>
2014-04-12 13:13:47 -0400

    Spotify demo work

--- MilkShake/MSSpotifyViewController.m
+++ MilkShake/MSSpotifyViewController.m
@@ -7,9 +7,16 @@
 //
 
 #import "MSSpotifyViewController.h"
+#import "AppDelegate.h"
+#import <Spotify/Spotify.h>
+
+// Constants
+static NSString * const kClientId = @"spotify-ios-sdk-beta";
+static NSString * const kCallbackURL = @"spotify-ios-sdk-beta://callback";
+static NSString * const kTokenSwapURL = @"http://localhost:1234/swap";
 
 @interface MSSpotifyViewController ()
-
+@property (nonatomic, readwrite) SPTTrackPlayer *trackPlayer;
 @end
 
 @implementation MSSpotifyViewController
@@ -33,24 +40,74 @@
     [closeButton setTitle:@"Close" forState:UIControlStateNormal];
     [self.view addSubview:closeButton];
     [closeButton addTarget:self action:@selector(closeView)forControlEvents:UIControlEventTouchUpInside];
+    
+    // Create SPTAuth instance; create login URL and open it
+    SPTAuth *auth = [SPTAuth defaultInstance];
+    NSURL *loginURL = [auth loginURLForClientId:kClientId
+                            declaredRedirectURL:[NSURL URLWithString:kCallbackURL]
+                                         scopes:@[@"login"]];
+    // Opening a URL in Safari close to application launch may trigger
+    // an iOS bug, so we wait a bit before doing so.
+    
+    [[UIApplication sharedApplication] performSelector:@selector(openURL:) withObject:loginURL afterDelay:0.1];
+
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(BOOL)openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    
+    // Ask SPTAuth if the URL given is a Spotify authentication callback
+    if ([[SPTAuth defaultInstance] canHandleURL:url withDeclaredRedirectURL:[NSURL URLWithString:kCallbackURL]]) {
+        
+        // Call the token swap service to get a logged in session
+        [[SPTAuth defaultInstance]
+         handleAuthCallbackWithTriggeredAuthURL:url
+         tokenSwapServiceEndpointAtURL:[NSURL URLWithString:kTokenSwapURL]
+         callback:^(NSError *error, SPTSession *session) {
+             
+             if (error != nil) {
+                 NSLog(@"*** Auth error: %@", error);
+                 return;
+             }
+             
+             // Call the -playUsingSession: method to play a track
+             [self playUsingSession:session];
+         }];
+        return YES;
+    }
+    
+    return NO;
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void)playUsingSession:(SPTSession *)session {
+    
+    // Create a new track player if needed
+    if (self.trackPlayer == nil) {
+        self.trackPlayer = [[SPTTrackPlayer alloc] initWithCompanyName:@"Your-Company-Name"
+                                                               appName:@"MilkShake"];
+    }
+    
+    [self.trackPlayer enablePlaybackWithSession:session callback:^(NSError *error) {
+        
+        if (error != nil) {
+            NSLog(@"*** Enabling playback got error: %@", error);
+            return;
+        }
+        
+        [SPTRequest requestItemAtURI:[NSURL URLWithString:@"spotify:album:4L1HDyfdGIkACuygktO7T7"]
+                         withSession:nil
+                            callback:^(NSError *error, SPTAlbum *album) {
+                                
+                                if (error != nil) {
+                                    NSLog(@"*** Album lookup got error %@", error);
+                                    return;
+                                }
+                                
+                                [self.trackPlayer playTrackProvider:album];
+                                
+                            }];
+    }];
+    
 }
-*/
 
 -(void)closeView
 {
