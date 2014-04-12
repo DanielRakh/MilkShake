09ae7b13791095145e79ab774c161ff0ff4dd8ba
Daniel Rakhamimov <danielrakh@gmail.com>
2014-04-12 12:21:56 -0400

    Initial Commit

---
+++ MilkShake/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MilkShake
+//
+//  Created by Daniel Rakhamimov on 4/12/14.
+//  Copyright (c) 2014 Daniel Rakhamimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
