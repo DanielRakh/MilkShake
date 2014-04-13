0c2d73f2d804beca86a5fdafe2d6de7fd20df27a
David Weissler <david.weissler@gmail.com>
2014-04-13 12:15:20 -0400

    Play only songs available in the US.

--- MilkShake/MSSpotifySearchResultsCollectionController.m
+++ MilkShake/MSSpotifySearchResultsCollectionController.m
@@ -13,6 +13,7 @@
 
 @interface MSSpotifySearchResultsCollectionController () <UICollectionViewDataSource, UICollectionViewDelegate>
 @property (nonatomic, strong) NSArray *searchResults;
+@property (nonatomic, strong) NSMutableArray *availableResults;
 @property (nonatomic, strong) NSMutableDictionary *trackAndCoverDictionary;
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 
@@ -34,6 +35,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.searchResults = [NSArray array];
+    self.availableResults = [NSMutableArray array];
     self.trackAndCoverDictionary = [NSMutableDictionary dictionary];
 }
 
@@ -45,14 +47,14 @@
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
-    return self.searchResults.count;
+    return self.availableResults.count;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
     MSSongCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
     [cell setBackgroundColor:[UIColor blueColor]];
-    SPTTrack *track = (SPTTrack *)self.searchResults[indexPath.row];
+    SPTTrack *track = (SPTTrack *)self.availableResults[indexPath.row];
     SPTPartialArtist *partialArtist = track.artists[0];
 
     NSString *title = track.name;
@@ -70,7 +72,7 @@
 #pragma mark - UICollectionViewDelegate Protocol
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    SPTTrack *track = self.searchResults[indexPath.row];
+    SPTTrack *track = self.availableResults[indexPath.row];
     SPTPartialAlbum *album = track.album;
     NSInteger offset = track.trackNumber - 1;
     offset = (offset < 0) ? 0 : offset;
@@ -86,6 +88,7 @@
 - (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar
 {
     [self.searchBar resignFirstResponder];
+    [self.availableResults removeAllObjects]; // new search, new results
     
     NSString *searchString = searchBar.text;
     [SPTRequest performSearchWithQuery:searchString queryType:SPTQueryTypeTrack session:nil
@@ -96,11 +99,7 @@
                                   }
                                   if ([object isKindOfClass:[NSArray class]]) {
                                       [self setSearchResults:object];
-                                      for (SPTTrack *track in self.searchResults) {
-                                          [self loadAlbumArtForTrack:track];
-                                      }
-                                      [self.collectionView reloadData];
-                                      NSLog(@"%@",object);
+                                      [self filterForAvailableTracks]; // some songs not available in US
                                   }
                               }];
 }
@@ -123,10 +122,21 @@
                                             NSData *imageData = [[NSData alloc] initWithContentsOfURL:coverURL];
                                             NSString *key = [NSString stringWithFormat:@"%@",track.uri];
                                             [self.trackAndCoverDictionary setValue:[UIImage imageWithData:imageData] forKey:key];
-
                                             [self.collectionView reloadData];
                                         }
                                     }];
+}
+
+- (void)filterForAvailableTracks
+{
+    for (SPTTrack *track in self.searchResults) {
+        // For now, only make US songs available
+        if ([track.availableTerritories containsObject:@"US"]) {
+            [self.availableResults addObject:track];
+            [self loadAlbumArtForTrack:track];
+        }
+    }
+    
 }
 
 - (IBAction)testP2P:(id)sender {
