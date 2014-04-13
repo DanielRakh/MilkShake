c68e0284b4b5b6cd97e37f02bfc3bdd7fb5cbba2
David Weissler <david.weissler@gmail.com>
2014-04-13 13:56:30 -0400

    initial loading view file

---
+++ MilkShake/MSLoadingView.h
@@ -0,0 +1,13 @@
+//
+//  MSLoadingView.h
+//  MilkShake
+//
+//  Created by David Weissler on 4/13/14.
+//  Copyright (c) 2014 Daniel Rakhamimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSLoadingView : UIView
+
+@end
---
+++ MilkShake/MSLoadingView.m
@@ -0,0 +1,31 @@
+//
+//  MSLoadingView.m
+//  MilkShake
+//
+//  Created by David Weissler on 4/13/14.
+//  Copyright (c) 2014 Daniel Rakhamimov. All rights reserved.
+//
+
+#import "MSLoadingView.h"
+
+@implementation MSLoadingView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
